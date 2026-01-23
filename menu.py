cdb7d638ea4b32959a858b141af077fdf901382f
xorriik <kirilchikmihail4@gmail.com>
2026-01-23 22:31:07 +0300

    make proto of menu using arcade.gui

--- menu.py
+++ menu.py
@@ -2,26 +2,47 @@ import arcade
 from pyglet.graphics import Batch
 from constants.window import SCREEN_HEIGHT, SCREEN_WIDTH
 from game import GameView
+from arcade.gui import UITextureButton, UIManager, UILabel
+from arcade.gui.widgets.layout import UIAnchorLayout, UIBoxLayout
 
 
 class MenuView(arcade.View):
     def on_show_view(self):
         self.batch = Batch()
         arcade.set_background_color(arcade.color.BLACK)
+        self.manager = UIManager()
+        self.manager.enable()
+        self.but_texture, self.but_texture_hovered = arcade.SpriteSheet(
+            'assets/ui/button_start.png'
+        ).get_texture_grid(
+            (20, 6),
+            columns=2,
+            count=2
+        )
+        self.anchor_layout = UIAnchorLayout()
+        self.box_layout = UIBoxLayout(vertical=True, space_between=20)
+        self.label = UILabel(
+            font_size=50,
+            text_color=arcade.color.WHITE,
+            text="unt1tl3d",
+            width=200,
+            align="center"
+        )
+        self.button = UITextureButton(
+            texture=self.but_texture,
+            texture_hovered=self.but_texture_hovered,
+            scale=15.0
+        )
+        self.button.on_click = self.change_view
+        self.box_layout.add(self.label)
+        self.box_layout.add(self.button)
+        self.anchor_layout.add(self.box_layout)
+        self.manager.add(self.anchor_layout)
     
     def on_draw(self):
-        text = arcade.Text(
-            'heres title pon da',
-            SCREEN_WIDTH // 2,
-            SCREEN_HEIGHT // 2,
-            arcade.color.WHITE,
-            20,
-            batch=self.batch,
-            anchor_x='center',
-            anchor_y='center'
-        )
         self.batch.draw()
+        self.manager.draw()
     
-    def on_key_press(self, key, modifiers):
+    def change_view(self, event):
         game_view = GameView()
         self.window.show_view(game_view)
